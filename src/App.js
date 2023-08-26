06635fb79d373133ea54a51c2282dbfdbdb809d6
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-27 03:14:27 +0530

    value and onChange added to inputs

--- src/App.js
+++ src/App.js
@@ -11,24 +11,26 @@ function App()
   const [nameError, setNameError] = useState('');
   const [rollNoError, setRollNoError] = useState('');
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className='app'>
       <h1>Student Attendance Portal</h1>
       <div className='container'>
         <img src='https://images.unsplash.com/photo-1567168544646-208fa5d408fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80' />
-          <form>
+          <form onSubmit={handleSubmit}>
             <h2>Please enter your details to login</h2>
 
             <label htmlFor='email'>Email ID:</label>
-            <input type='email' id="email" required/>
+            <input type='email' id="email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
 
             <label htmlFor='name'>Name:</label>
-            <input type='text' id="name" required/>
+            <input type='text' id="name" required value={name} onChange={(e) => setName(e.target.value)}/>
 
             <label htmlFor='rollNo'>Roll No:</label>
-            <input type='text' id='rollNo' required/>
+            <input type='text' id='rollNo' required value={rollNo} onChange={(e) => setRollNo(e.target.value)}/>
 
             <div className='formBtn'>
               <button id='loginBtn' type='submit'>Login</button>
