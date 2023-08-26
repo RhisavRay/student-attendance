18e446d4cfe6977e7dc5ca61191df2b800b93731
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-27 03:05:14 +0530

    style updates

--- src/App.js
+++ src/App.js
@@ -11,18 +11,25 @@ function App()
   const [nameError, setNameError] = useState('');
   const [rollNoError, setRollNoError] = useState('');
 
+
+
   return (
     <div className='app'>
+      <h1>Student Attendance Portal</h1>
       <div className='container'>
         <img src='https://images.unsplash.com/photo-1567168544646-208fa5d408fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80' />
           <form>
             <h2>Please enter your details to login</h2>
+
             <label htmlFor='email'>Email ID:</label>
-            <input type='email' id="email" required></input>
+            <input type='email' id="email" required/>
+
             <label htmlFor='name'>Name:</label>
-            <input type='text' id="name" required></input>
+            <input type='text' id="name" required/>
+
             <label htmlFor='rollNo'>Roll No:</label>
-            <input type='text' id='rollNo' required></input>
+            <input type='text' id='rollNo' required/>
+
             <div className='formBtn'>
               <button id='loginBtn' type='submit'>Login</button>
               <button id='clearBtn' type='reset'>Clear Form</button>
