e6087865adc2cd5123ce1922290b70d3083c4989
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-27 14:04:45 +0530

    Fool proofing done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function Home()
 {
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isSubject, setIsSubject] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,31 +20,31 @@ function Home()
             <div className="form-section">
                     <label htmlFor="subject">Subject</label>
                     <select id="subject" required>
-                        <option>*SELECT AN OPTION*</option>
-                        <option>Operating System</option>
-                        <option>Database Managemen System</option>
-                        <option>Forensic Investigation and Research</option>
-                        <option>Robotics</option>
-                        <option>Security Governance</option>
+                        <option value="">*SELECT AN OPTION*</option>
+                        <option value="OS">Operating System</option>
+                        <option value="DBMS">Database Managemen System</option>
+                        <option value="FIR">Forensic Investigation and Research</option>
+                        <option value="Rob">Robotics</option>
+                        <option value="SG">Security Governance</option>
                     </select>
                 </div>
 
                 <div className="form-section">
                     <label htmlFor="time">Class timing</label>
                     <select id="time" required>
-                        <option>*SELECT AN OPTION*</option>
-                        <option>P1 - 8:00 to 8:50</option>
-                        <option>P2 - 8:50 to 9:40</option>
-                        <option>P3 - 9:45 to 10:35</option>
-                        <option>P4 - 10:40 to 11:30</option>
-                        <option>P5 - 11:35 to 12:25</option>
-                        <option>P6 - 12:30 to 13:20</option>
-                        <option>P7 - 13:25 to 14:15</option>
-                        <option>P8 - 14:20 to 15:10</option>
-                        <option>P9 - 15:10 to 16:00</option>
-                        <option>P10 - 16:00 to 16:50</option>
-                        <option>L1 - 16:50 to 17:30</option>
-                        <option>L2 - 17:30 to 18:10</option>
+                        <option value="">*SELECT AN OPTION*</option>
+                        <option value="P1">P1 - 8:00 to 8:50</option>
+                        <option value="P2">P2 - 8:50 to 9:40</option>
+                        <option value="P3">P3 - 9:45 to 10:35</option>
+                        <option value="P4">P4 - 10:40 to 11:30</option>
+                        <option value="P5">P5 - 11:35 to 12:25</option>
+                        <option value="P6">P6 - 12:30 to 13:20</option>
+                        <option value="P7">P7 - 13:25 to 14:15</option>
+                        <option value="P8">P8 - 14:20 to 15:10</option>
+                        <option value="P9">P9 - 15:10 to 16:00</option>
+                        <option value="P10">P10 - 16:00 to 16:50</option>
+                        <option value="L1">L1 - 16:50 to 17:30</option>
+                        <option value="L2">L2 - 17:30 to 18:10</option>
                     </select>
                 </div>
 
@@ -55,6 +56,7 @@ function Home()
                 <button type="submit" id="loginBtn">Present!</button>
                 <button type="reset" id="clearBtn">Clear form</button>
             </form>
+
             {isSubmitted && (
         <div className="confirmation-message">
           Submission successful!
