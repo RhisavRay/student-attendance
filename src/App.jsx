7d71c033a9ebeb0f64c6b40d1f61b0138e6b20fb
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-29 04:12:46 +0530

    Redundent code removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Login from "./components/Login";
-import Dashboard from "./components/ViewAtt";
 import Home from "./components/Home";
 
 import { useState } from "react";
--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react"
-
-import Login from "./Login";
-
 function LogoutSwitch({logout})
 {
   return (
